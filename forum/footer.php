c85d78861d3287225ff34f7e7d127005e931c302
WenKit99 <93528809+WenKit99@users.noreply.github.com>
2022-01-15 00:20:47 +0800

    Add files via upload

--- forum/footer.php
+++ forum/footer.php
@@ -1,4 +1,10 @@
 </div><!-- content -->
+<!DOCTYPE html>
+<head>
+    <br>
+    <?php echo "Scan the QR code to follow Facebook page for more information.";?>
+   <p><?php include 'C:\\xampp\\htdocs\\WebDevProject-main\\WebDevProject-main\\qr_code-V2\\social_profile.php';?></p>
+</head>
 <div id="footer"></div>
 </div>
 </div>
