4d67a9c834e1144c05e6413460c80021318f7852
ARYN-27 <53664593+ARYN-27@users.noreply.github.com>
2022-01-15 06:26:10 +0800

    QR V3 Done ✔

--- forum/footer.php
+++ forum/footer.php
@@ -1,10 +1,11 @@
 </div><!-- content -->
 <!DOCTYPE html>
-<head>
+<!--
+<div>
     <br>
     <?php echo "Scan the QR code to follow Facebook page for more information.";?>
    <p><?php include 'C:\\xampp\\htdocs\\WebDevProject-main\\WebDevProject-main\\qr_code-V2\\social_profile.php';?></p>
-</head>
+</head> -->
 <div id="footer"></div>
 </div>
 </div>
