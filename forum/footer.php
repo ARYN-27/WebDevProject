b72aedefc6cadf94d7cc93788f23c9c510478a70
ARYN-27 <53664593+ARYN-27@users.noreply.github.com>
2022-01-15 20:57:43 +0800

    footer.php cleanup ✔

--- forum/footer.php
+++ forum/footer.php
@@ -1,11 +1,6 @@
 </div><!-- content -->
 <!DOCTYPE html>
-<!--
-<div>
-    <br>
-    <?php echo "Scan the QR code to follow Facebook page for more information.";?>
-   <p><?php include 'C:\\xampp\\htdocs\\WebDevProject-main\\WebDevProject-main\\qr_code-V2\\social_profile.php';?></p>
-</head> -->
+
 <div id="footer"></div>
 </div>
 </div>
